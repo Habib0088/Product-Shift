2f17ef00bfb90fae0121ffb8aebc953d7a6e8907
Habib0088 <sarkerbatch2k19@gmail.com>
2025-11-15 22:39:42 +0600

    Route setup done

---
+++ src/Route/Route.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "../layout/Layout";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Layout,
+    children:[
+        {
+            index:true,
+            Component:Home
+        }
+    ]
+  },
+]);
